d1e75bc4b003ba6b2ab62944a1785d57e652c7ab
Shannon <hwangys@mit.edu>
2020-06-21 14:33:53 -0700

    grad school update and design revamp

--- action.js
+++ action.js
@@ -1,9 +1,11 @@
 window.addEventListener('load', function () {
+    // rotate the site headings
     var rotates = document.getElementsByClassName('h1sideways');
     for (var i = 0; i < rotates.length; i++) {
         rotates[i].style.height = rotates[i].offsetWidth + 'px';
     }
 
+    // add accordion for classes
     var acc = document.getElementById("accordion");
     acc.onclick = function() {
         acc.classList.toggle("active");
@@ -17,35 +19,72 @@ window.addEventListener('load', function () {
         }
     }
 
-    $('a[href*=\\#]').on('click', function(event){
+    // class bolding
+    // $(".category-filter .category").on("click", function(event) {
+
+    //   var selected = $(event.target).hasClass("active");
+      
+    //   if (!selected) {
+    //     var category = $(event.target).attr("category");
+    //     $(".category-filter .category").removeClass("active");
+    //     $(event.target).addClass("active");
+
+    //     // if (category == 'all') {
+    //     //   setTimeout(function(){
+    //     //     $(".project").fadeIn(300);
+    //     //   },300);
+    //     // }
+    //     // else {
+    //     //   setTimeout(function(){
+    //     //     $(".category-" + category).fadeIn(300);
+    //     //   },300);
+    //     // }
+    //   }
+    // });
+
+    // add accordions for projects
+    var projacc = document.getElementsByClassName("projectaccordion");
+    // var projpanels = document.getElementsByClassName("projectpanel");
+    var i;
+    for (i = 0; i < projacc.length; i++) {
+      projacc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var projpanel = this.nextElementSibling;
+        if (projpanel.style.maxHeight) {
+          projpanel.style.maxHeight = null;
+        } else {
+          projpanel.style.maxHeight = projpanel.scrollHeight + "px";
+        } 
+      });
+    }
+
+  // project filter, adapted from Shannon Peng's website
+	$(".category-filter .category").on("click", function(event) {
+
+    var selected = $(event.target).hasClass("active");
+    
+		if (!selected) {
+			var category = $(event.target).attr("category");
+			$(".category-filter .category").removeClass("active");
+			$(event.target).addClass("active");
+      $(".project").fadeOut(300);
+      
+			if (category == 'all') {
+				setTimeout(function(){
+					$(".project").fadeIn(300);
+				},300);
+			}
+			else {
+				setTimeout(function(){
+					$(".category-" + category).fadeIn(300);
+				},300);
+			}
+		}
+  });
+  
+  // shortcut to scroll to sections
+  $('a[href*=\\#]').on('click', function(event){
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
     });
 });
-
-function openModal(modalID, element) {
-  document.getElementById(modalID).style.display = "block";
-  showSlides(1, element);
-}
-// Close the Modal
-function closeModal(modalID) {
-  document.getElementById(modalID).style.display = "none";
-}
-
-var slideIndex = 1;
-
-// Next/previous controls
-function plusSlides(n, element) {
-  showSlides(slideIndex += n, element);
-}
-
-function showSlides(n, element) {
-  var i;
-  var slides = document.getElementsByClassName(element);
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "flex";
-}
